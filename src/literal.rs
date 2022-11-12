367842615879fa7d11b832b638038ea7bdda5ea1
Sam Brockie <sambrockie@icloud.com>
2022-11-12 22:37:00 +0000

    Add Identifier variant to Literal enum

--- src/literal.rs
+++ src/literal.rs
@@ -2,15 +2,17 @@ use std::fmt;
 
 #[derive(Debug, PartialEq)]
 pub enum Literal {
+    Identifier(String),
+    Number(f64),
     String(String),
-    Number(isize),
 }
 
 impl fmt::Display for Literal {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match &*self {
-            Literal::String(value) => write!(f, "{:?}", value),
+            Literal::Identifier(value) => write!(f, "{:?}", value),
             Literal::Number(value) => write!(f, "{:?}", value),
+            Literal::String(value) => write!(f, "{:?}", value),
         }
     }
 }
