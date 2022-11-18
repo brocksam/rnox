07536cb5834ee3e8da6a7bce983ba9406d1dbc98
Sam Brockie <sambrockie@icloud.com>
2022-11-18 15:48:59 +0100

    Add enum to hold/represent Nox values

---
+++ src/value.rs
@@ -0,0 +1,20 @@
+use std::fmt;
+
+#[derive(Clone, Debug)]
+pub enum Value {
+    Nil,
+    Bool(bool),
+    Number(f64),
+    String(String),
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Nil => write!(f, "nil"),
+            Self::Bool(b) => write!(f, "{}", b),
+            Self::Number(n) => write!(f, "{}", n),
+            Self::String(s) => write!(f, "{}", s),
+        }
+    }
+}
