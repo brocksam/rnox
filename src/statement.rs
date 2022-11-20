76f7a8c484460af37cf94c7d2e2f4bd82bea52d8
Sam Brockie <sambrockie@icloud.com>
2022-11-20 11:17:34 +0100

    Introduce Variable variant of Statement

--- src/statement.rs
+++ src/statement.rs
@@ -1,7 +1,11 @@
-use crate::expr::Expr;
+use crate::{
+    expr::Expr,
+    token::Token,
+};
 
 #[derive(Debug, PartialEq)]
 pub enum Statement {
     Print(Expr),
     Expression(Expr),
+    Variable(Token, Expr),
 }
